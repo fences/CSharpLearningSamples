162f2c9e6cef5339a33738fe73e9af06c07f4613
Alireza <Alireza@DESKTOP-QC6TL9A>
2026-02-11 08:58:35 +0330

    New Project

---
+++ CSharpLearingDay5-S1/Sample1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace CSharpLearingDay5_S1
+{
+    partial class Sample1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnEStop = new System.Windows.Forms.Button();
+            this.tmrMain = new System.Windows.Forms.Timer(this.components);
+            this.lblState = new System.Windows.Forms.Label();
+            this.prgConveyor = new System.Windows.Forms.ProgressBar();
+            this.prgBottle = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(15, 35);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(115, 45);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnEStop
+            // 
+            this.btnEStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEStop.Location = new System.Drawing.Point(257, 35);
+            this.btnEStop.Name = "btnEStop";
+            this.btnEStop.Size = new System.Drawing.Size(203, 45);
+            this.btnEStop.TabIndex = 1;
+            this.btnEStop.Text = "Emergency Stop";
+            this.btnEStop.UseVisualStyleBackColor = true;
+            this.btnEStop.Click += new System.EventHandler(this.btnEStop_Click);
+            // 
+            // tmrMain
+            // 
+            this.tmrMain.Tick += new System.EventHandler(this.tmrMain_Tick);
+            // 
+            // lblState
+            // 
+            this.lblState.AutoSize = true;
+            this.lblState.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblState.Location = new System.Drawing.Point(97, 183);
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(16, 20);
+            this.lblState.TabIndex = 2;
+            this.lblState.Text = "-";
+            // 
+            // prgConveyor
+            // 
+            this.prgConveyor.Location = new System.Drawing.Point(12, 363);
+            this.prgConveyor.Name = "prgConveyor";
+            this.prgConveyor.Size = new System.Drawing.Size(448, 47);
+            this.prgConveyor.TabIndex = 3;
+            // 
+            // prgBottle
+            // 
+            this.prgBottle.Location = new System.Drawing.Point(12, 283);
+            this.prgBottle.Name = "prgBottle";
+            this.prgBottle.Size = new System.Drawing.Size(448, 27);
+            this.prgBottle.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 340);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Conveyor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 260);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "FILLING";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(136, 35);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(115, 45);
+            this.btnReset.TabIndex = 7;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Status:";
+            // 
+            // Sample1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 443);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.prgBottle);
+            this.Controls.Add(this.prgConveyor);
+            this.Controls.Add(this.lblState);
+            this.Controls.Add(this.btnEStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Sample1";
+            this.Text = "Sample1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnEStop;
+        private System.Windows.Forms.Timer tmrMain;
+        private System.Windows.Forms.Label lblState;
+        private System.Windows.Forms.ProgressBar prgConveyor;
+        private System.Windows.Forms.ProgressBar prgBottle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ CSharpLearingDay5-S1/Sample1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpLearingDay5_S1
+{
+    public partial class Sample1 : Form
+    {
+        public Sample1()
+        {
+            InitializeComponent();
+        }
+
+        // ==========================================
+        // 1. GLOBAL VARIABLES (حافظه سیستم)
+        // ==========================================
+
+        // متغیر مرحله (State Machine):
+        // 0 = Stop/Idle (بیکار)
+        // 1 = Moving In (ورود بطری به زیر نازل)
+        // 2 = Filling (پر کردن بطری)
+        // 3 = Moving Out (خروج بطری)
+        int _step = 0;
+
+        // پرچم وضعیت اضطراری
+        bool _isEmergency = false;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // ==========================================
+        // 2. BUTTON EVENTS (دکمه‌ها)
+        // ==========================================
+
+        // دکمه شروع چرخه
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // ایمنی (Interlock): اگر وضعیت اضطراری فعال است، اجازه استارت نده
+            if (_isEmergency == true)
+            {
+                MessageBox.Show("System is in EMERGENCY mode! Please Reset first.", "Safety Error");
+                return; // خروج از متد (کد پایین اجرا نمی‌شود)
+            }
+
+            // تنظیم شرایط اولیه
+            _step = 1; // رفتن به مرحله اول (حرکت)
+            tmrMain.Interval = 100; // سرعت عادی
+            tmrMain.Start(); // روشن کردن تایمر
+
+            // آپدیت UI
+            lblState.Text = "STATE: MOVING IN...";
+            lblState.BackColor = Color.LightYellow;
+        }
+
+        // دکمه توقف اضطراری (Emergency Stop)
+        private void btnEStop_Click(object sender, EventArgs e)
+        {
+            // 1. قطع فوری تایمر (مهمترین کار)
+            tmrMain.Stop();
+
+            // 2. فعال کردن پرچم ایمنی
+            _isEmergency = true;
+
+            // 3. تغییر وضعیت ظاهری برای هشدار
+            this.BackColor = Color.Red; // کل فرم قرمز شود
+            lblState.Text = "!!! EMERGENCY STOP !!!";
+            lblState.ForeColor = Color.White;
+            lblState.BackColor = Color.DarkRed;
+        }
+
+
+        // ==========================================
+        // 3. TIMER LOGIC (مغز متفکر سیستم)
+        // ==========================================
+        private void tmrMain_Tick(object sender, EventArgs e)
+        {
+            // استفاده از switch برای مدیریت مراحل (Sequence Control)
+            switch (_step)
+            {
+                case 1: // --- STAGE 1: MOVING IN (ورود بطری) ---
+                    // حرکت نوار نقاله
+                    if (prgConveyor.Value < 50)
+                    {
+                        prgConveyor.Value += 2; // سرعت حرکت
+                    }
+                    else
+                    {
+                        // شرط گذار (Transition Logic): رسیدن به زیر نازل
+                        _step = 2; // برو به مرحله بعد
+                        lblState.Text = "STATE: FILLING...";
+                        lblState.BackColor = Color.LightBlue;
+                    }
+                    break;
+
+                case 2: // --- STAGE 2: FILLING (پر کردن) ---
+                    // نوار نقاله ثابت می‌ماند، بطری پر می‌شود
+                    if (prgBottle.Value < 100)
+                    {
+                        prgBottle.Value += 2; // سرعت پر شدن
+                    }
+                    else
+                    {
+                        // شرط گذار: بطری پر شد
+                        _step = 3; // برو به مرحله بعد
+                        lblState.Text = "STATE: MOVING OUT...";
+                        lblState.BackColor = Color.LightYellow;
+                    }
+                    break;
+
+                case 3: // --- STAGE 3: MOVING OUT (خروج بطری) ---
+                    // ادامه حرکت نوار نقاله از 50 به 100
+                    if (prgConveyor.Value < 100)
+                    {
+                        prgConveyor.Value += 2;
+                    }
+                    else
+                    {
+                        // پایان سیکل (Cycle Complete)
+                        // آماده‌سازی برای بطری بعدی (Loop)
+
+                        prgConveyor.Value = 0; // نوار ریست شود
+                        prgBottle.Value = 0;   // بطری جدید خالی است
+
+                        _step = 1; // بازگشت به مرحله اول
+                        lblState.Text = "STATE: MOVING IN (Next Bottle)...";
+                    }
+                    break;
+            }
+        }
+
+        // دکمه ریست (برای بازگرداندن سیستم به حالت عادی)
+        // *دانشجو باید این دکمه را اضافه کند یا کدش را بداند*
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            // بازگرداندن متغیرها به حالت پیش‌فرض
+            _isEmergency = false;
+            _step = 0;
+
+            // صفر کردن نوارها
+            prgConveyor.Value = 0;
+            prgBottle.Value = 0;
+
+            // بازگرداندن رنگ‌ها به حالت عادی
+            this.BackColor = SystemColors.Control;
+            lblState.Text = "STATE: READY";
+            lblState.ForeColor = Color.Black;
+            lblState.BackColor = Color.LightGray;
+        }
+    }
+}
